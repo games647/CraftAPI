dfaefd6a778b94ea9c778dd167b44e484f0ca4f1
games647 <games647@users.noreply.github.com>
2018-04-06 17:40:20 +0200

    Add human readable names to the skin models

--- src/main/java/com/github/games647/craftapi/model/skin/Model.java
+++ src/main/java/com/github/games647/craftapi/model/skin/Model.java
@@ -8,14 +8,27 @@ import com.google.gson.annotations.SerializedName;
 public enum Model {
 
     /**
-     * Default model with square arms (Steven style)
+     * Default model with square arms (Steve style)
      */
-    SQUARE,
+    SQUARE("Steve"),
 
     /**
      * Slim model with slim arms (Alex style)
      */
+    //force lowercase
     @SerializedName("slim")
-    SLIM
+    SLIM("Alex");
 
+    private final String name;
+
+    Model(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return skin model representation names like Steve for square arms and Alex for slim arms
+     */
+    public String getName() {
+        return name;
+    }
 }
