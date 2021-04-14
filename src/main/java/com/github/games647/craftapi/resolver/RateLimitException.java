02cc5c4dbb040fc15e2e4080f0f7446ede21859a
games647 <games647@users.noreply.github.com>
2021-04-14 17:47:22 +0200

    Disable stacktraces for rate limit exceptions
    Rate limits are likely to occur more often than other exceptions.
    Especially considering an bot attack, this could have a performance
    effect for filling out the stacktraces.

--- src/main/java/com/github/games647/craftapi/resolver/RateLimitException.java
+++ src/main/java/com/github/games647/craftapi/resolver/RateLimitException.java
@@ -10,14 +10,14 @@ public class RateLimitException extends Exception {
     public static final int RATE_LIMIT_RESPONSE_CODE = 429;
 
     public RateLimitException() {
-        super("Too many requests");
+        super("Too many requests", null, true, false);
     }
 
     public RateLimitException(String playerName) {
-        super("Too many requests for the UUID of player " + playerName);
+        super("Too many requests for the UUID of player " + playerName, null, true, false);
     }
 
     public RateLimitException(UUID skinId) {
-        super("Too many requests for skin " + skinId);
+        super("Too many requests for skin " + skinId, null, true, false);
     }
 }
