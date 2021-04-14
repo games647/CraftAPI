239705e3977a8f1deea6bccc97db6d148f837d5b
games647 <games647@users.noreply.github.com>
2021-04-14 18:33:02 +0200

    Disable Javadoc linting

--- src/main/java/com/github/games647/craftapi/resolver/RateLimitException.java
+++ src/main/java/com/github/games647/craftapi/resolver/RateLimitException.java
@@ -9,14 +9,23 @@ public class RateLimitException extends Exception {
 
     public static final int RATE_LIMIT_RESPONSE_CODE = 429;
 
+    /**
+     * Generic rate limitation
+     */
     public RateLimitException() {
         super("Too many requests", null, true, false);
     }
 
+    /**
+     * Rate limitation for the given player name
+     */
     public RateLimitException(String playerName) {
         super("Too many requests for the UUID of player " + playerName, null, true, false);
     }
 
+    /**
+     * Rate limit for skin download of the specified account UUID
+     */
     public RateLimitException(UUID skinId) {
         super("Too many requests for skin " + skinId, null, true, false);
     }
